0fde3f54aafd299712e864a7b1d2d35807dd9d17
dani <danifromecuador@gmail.com>
2023-06-29 12:36:21 -0500

    Fetch titles of the movies from the API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('hello console');
+const loadMovies = async () => {
+  try {
+    const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=490505c7fe17d32a465ca75403c13a33");
+    // console.log(response);
+    if (response.status === 200) {
+      const data = await response.json();
+      // console.log(data.results);
+      let movies = "";
+      data.results.forEach(movie => movies += `<div class="movie">${movie.title}</div>`);
+      console.log(movies);
+      document.getElementById("mainContainer").innerHTML = movies;
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+};
+
+loadMovies();
